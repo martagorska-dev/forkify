fbb0cbcea964c5da3b4b8dc991a37cfcb2df3204
martagorska-dev <martagorska.dev@gmail.com>
2023-04-18 12:47:58 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4a401561-0f27-40aa-936d-eb0f8fef7cef';
+export const MODAL_CLOSE_SEC = 2.5;
